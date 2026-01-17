6fc2c99d490d59437b1193503f3601db00624a0f
vfedotova418-png <vfedotova418@gmail.com>
2026-01-17 12:43:00 +0300

    Fixed local repository issues

---
+++ backend/Program/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name:");
+        string? name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}");
+        Console.WriteLine("How are you?");
+        Console.WriteLine(1+1);
+    }
+}
